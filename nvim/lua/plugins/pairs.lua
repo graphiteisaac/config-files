081aac82555e69d79b981b9c3f10c6f67c548d9c
Isaac <isaac@grphcrtv.com>
2024-08-08 13:42:04 +1000

    add pairs plugin

--- nvim/lua/plugins/pairs.lua
+++ nvim/lua/plugins/pairs.lua
@@ -1,6 +1,14 @@
-return {}
---[[return {
-    'windwp/nvim-autopairs',
-    event = "InsertEnter",
-    config = true
-}]]
+return {
+    {
+        'windwp/nvim-ts-autotag',
+        opts = {
+            enable_close = true,
+            enable_rename = true,
+            enable_close_on_slash = true
+        },
+    },
+    {
+        'echasnovski/mini.pairs',
+        opts = {},
+    }
+}
