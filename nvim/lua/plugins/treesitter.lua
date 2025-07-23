5b6c2077b53d8cc4a46ae504cfd1c656796312ea
Isaac <isaac@grphcrtv.com>
2025-07-23 22:34:31 +1000

    improve tree-sitter config

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -11,12 +11,31 @@ return {
 		}
 	},
 	config = function()
+		local ts_parsers = {
+			-- web languages
+			'twig', 'gleam', 'typescript', 'javascript', 'vue', 'css', 'php', 'tsx', 'jsx', 'typescriptreact',
+			'javascriptreact', 'sql', 'hbs', 'handlebars',
+			-- fun and experimental
+			'pony', 'lisp', 'scheme',
+			-- systems, other, etc.
+			'go', 'rust', 'ocaml',
+			-- config and writing
+			'djot', 'yaml', 'json', 'lua', 'markdown'
+		}
+
+		local nts = require("nvim-treesitter")
+		nts.install(ts_parsers)
+
 		vim.api.nvim_create_autocmd('FileType', {
-			pattern = { 'twig', 'gleam', 'typescript', 'javascript', 'vue', 'css', 'go', 'rust', 'djot', 'php', 'pony', 'lisp', 'tsx', 'jsx', 'typescriptreact', 'javascriptreact', 'scheme', 'ocaml', 'sql' },
-			callback = function()
-				vim.treesitter.start()
-				vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
-				vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+			callback = function(args)
+				local filetype = args.match
+				local lang = vim.treesitter.language.get_lang(filetype)
+
+				if vim.treesitter.language.add(lang) then
+					vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+					vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+					vim.treesitter.start()
+				end
 			end,
 		})
 	end
