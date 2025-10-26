7cbb955a6419b847ef30dc8a37f96846b81e2df6
graphiteisaac <isaac@grphcrtv.com>
2025-10-26 11:12:04 +1100

    (term) make wezterm suppress that annoying warning

--- wezterm.lua
+++ wezterm.lua
@@ -3,27 +3,28 @@ local wezterm = require('wezterm')
 local config = wezterm.config_builder()
 
 config.font = wezterm.font_with_fallback {
-    'Maple Mono NF',
-    'Jetbrains Mono',
+	'Maple Mono NF',
+	'Jetbrains Mono',
 }
 config.color_scheme = 'Catppuccin Mocha'
 config.window_padding = {
-    left = 0,
-    right = 0,
-    top = 0,
-    bottom = 0
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0
 }
 
 config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
 
 config.mouse_bindings = {
-    {
-        event = { Down = { streak = 1, button = "Right" } },
-        mods = "NONE",
-        action = wezterm.action({ PasteFrom = "Clipboard" }),
-    },
+	{
+		event = { Down = { streak = 1, button = "Right" } },
+		mods = "NONE",
+		action = wezterm.action({ PasteFrom = "Clipboard" }),
+	},
 }
 
 config.audible_bell = "Disabled"
+config.warn_about_missing_glyphs = false
 
 return config
