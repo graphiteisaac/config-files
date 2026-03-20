d74c80e898908d422d8e6c4e2187de5d06f34584
graphiteisaac <isaac@grphcrtv.com>
2026-03-20 16:12:43 +1100

    (nvim) fall back to find_files if not in a git repo

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,11 +1,20 @@
 return {
-    'nvim-telescope/telescope.nvim',
+	'nvim-telescope/telescope.nvim',
 	branch = 'master',
-    keys = {
-        { '<leader>ff', '<cmd>Telescope git_files<CR>', desc = '(Telescope) Find Files (Git)' },
-        { '<leader>fg', '<cmd>Telescope live_grep<CR>', desc = '(Telescope) Grep' },
-        { '<leader>fb', '<cmd>Telescope buffers<CR>', desc = '(Telescope) Open Buffers' },
-        { '<leader>fd', '<cmd>Telescope diagnostics<CR>', desc = '(Telescope) Diagnostics' },
-        { '<leader>fh', '<cmd>Telescope oldfiles<CR>', desc = '(Telescope) Historic Files' },
-    },
+	keys = function()
+		local function find_files()
+			local ok = pcall(require('telescope.builtin').git_files, { show_untracked = true })
+			if not ok then
+				require('telescope.builtin').find_files()
+			end
+		end
+
+		return {
+			{ '<leader>ff', find_files,                       desc = '(Telescope) Find Files (Git)' },
+			{ '<leader>fg', '<cmd>Telescope live_grep<CR>',   desc = '(Telescope) Grep' },
+			{ '<leader>fb', '<cmd>Telescope buffers<CR>',     desc = '(Telescope) Open Buffers' },
+			{ '<leader>fd', '<cmd>Telescope diagnostics<CR>', desc = '(Telescope) Diagnostics' },
+			{ '<leader>fh', '<cmd>Telescope oldfiles<CR>',    desc = '(Telescope) Historic Files' },
+		}
+	end,
 }
