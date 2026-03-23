79c4603fd80deee51aaa9d115ac50d9b533cc3ae
graphiteisaac <isaac@grphcrtv.com>
2026-03-23 11:12:14 +1100

    (chore) delete old files

--- install.sh
+++
@@ -1,9 +0,0 @@
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-# Install neovim
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
-chmod u+x nvim.appimage
-./nvim.appimage
-
-# Install Bat
-sudo apt install bat
